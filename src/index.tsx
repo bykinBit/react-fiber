c2c3867293420359cd3234c71bcf13c280f7f0aa
金宝玉 <jinbaoyu@arcgine.com>
2025-12-11 17:50:24 +0800

    数据验证测试

--- src/index.tsx
+++ src/index.tsx
@@ -1,77 +1,77 @@
 import React from "./react";
 import ReactDOM from "./react-dom/client";
 
-class Counter extends React.Component{
-    static defaultProps={
-        name:'hello'
+class Counter extends React.Component {
+    static defaultProps = {
+        name: 'hello'
     }
-    constructor(props:any){
+    constructor(props: any) {
         super(props);
-        this.state={number:0}
+        this.state = { number: 0 }
         console.log('Counter 1.constructor');
     }
-    UNSAFE_componentWillMount(){
+    UNSAFE_componentWillMount() {
         console.log('Counter 2.componentWillMount');
     }
-    handleClick=()=>{
-        this.setState({number:this.state.number+1})
+    handleClick = () => {
+        this.setState({ number: this.state.number + 1 })
     }
-    shouldComponentUpdate(nextProps:any,nextState:any){
+    shouldComponentUpdate(nextProps: any, nextState: any) {
         console.log('Counter 5.shouldComponentUpdate');
-        return nextState.number%2===0;
+        return nextState.number % 2 === 0;
     }
-    UNSAFE_componentWillUpdate(){
+    UNSAFE_componentWillUpdate() {
         console.log('Counter 6.componentWillUpdate');
     }
-    componentDidUpdate(){
+    componentDidUpdate() {
         console.log('Counter 7.componentDidUpdate');
     }
-    render(){
+    render() {
         console.log('Counter 3.render');
         return (
             <div>
                 <p>number:{this.state.number}</p>
                 {
-                    this.state.number===4?null:<FunctionCounter count={this.state.number}/>
+                    this.state.number === 4 ? null : <ChildCounter count={this.state.number} />
                 }
                 <button onClick={this.handleClick}>+</button>
             </div>
         )
     }
-    componentDidMount(){
+    componentDidMount() {
         console.log('Counter 4.componentDidMount');
     }
 }
-function FunctionCounter(props:any){
+function FunctionCounter(props: any) {
     return (
         <div>{props.count}</div>
     )
 }
-class ChildCounter extends React.Component{
-    UNSAFE_componentWillReceiveProps(newProps:any){
+class ChildCounter extends React.Component {
+    UNSAFE_componentWillReceiveProps(newProps: any) {
         console.log('ChildCounter 4.UNSAFE_componentWillReceiveProps');
     }
-    UNSAFE_componentWillMount(){
+    UNSAFE_componentWillMount() {
         console.log('ChildCounter 1.UNSAFE_componentWillMount');
     }
-    shouldComponentUpdate(nextProps:any,nextState:any){
+    shouldComponentUpdate(nextProps: any, nextState: any) {
         console.log('ChildCounter 5.UNSAFE_componentWillMount');
-        return nextProps.count%3===0;
+        return nextProps.count % 3 === 0;
     }
-    render(){
+    render() {
         console.log('ChildCounter 2.render')
         return (
             <p>childCount:{this.props.count}</p>
         );
     }
-    componentDidMount(){
+    componentDidMount() {
         console.log('ChildCounter 3.componentDidMount');
     }
-    UNSAFE_componentWillUnmount(){
+    UNSAFE_componentWillUnmount() {
         console.log('ChildCounter 6.UNSAFE_componentWillUnmount');
     }
 }
-const element = <Counter age={16}/>;
+const element = <Counter age={16} />;
 console.log(element);
 
 const DOMRoot = ReactDOM.createRoot(document.getElementById("root"));
